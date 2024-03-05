bf8473614fadc796afd43f62f3511ad0533fa052
twntysmth <161054857+calvin-kamp@users.noreply.github.com>
2024-03-06 00:58:23 +0100

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,27 @@ import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import netlify from "@astrojs/netlify";
 
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
-            components: {
-                page: 'storyblok/Page',
-                meta: 'storyblok/Meta',
-                image_text: 'components/modules/ImageText'
-            },
-        }),
-    ],
+    integrations: [storyblok({
+        accessToken: env.STORYBLOK_TOKEN,
+        components: {
+            page: 'storyblok/Page',
+            meta: 'storyblok/Meta',
+            image_text: 'components/modules/ImageText'
+        }
+    })],
     vite: {
         plugins: [basicSsl()],
         server: {
-            https: true,
-        },
+            https: true
+        }
     },
+    output: "server",
+    adapter: netlify()
 });
