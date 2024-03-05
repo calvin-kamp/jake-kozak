9ff8ef232bc7c30ef3e60504b927171ee5b4123e
twntysmth <161054857+calvin-kamp@users.noreply.github.com>
2024-03-06 00:52:35 +0100

    - some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                page: 'storyblok/Page',
+                meta: 'storyblok/Meta',
+                image_text: 'components/modules/ImageText',
+                image_banner: 'components/ImageBanner',
+                hero_banner: 'components/HeroBanner',
+            },
+        }),
+    ],
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true,
+        },
+    },
+});
