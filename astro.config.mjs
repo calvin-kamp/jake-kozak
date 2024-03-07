326454d6e8ab9609a4377947ed8b767399a527c0
twntysmth <161054857+calvin-kamp@users.noreply.github.com>
2024-03-07 13:21:16 +0100

    swaped from netlify to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,27 +3,29 @@ import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import netlify from "@astrojs/netlify";
-
+import vercel from "@astrojs/vercel/serverless";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [storyblok({
-        accessToken: env.STORYBLOK_TOKEN,
-        components: {
-            page: 'storyblok/Page',
-            meta: 'storyblok/Meta',
-            image_text: 'components/modules/ImageText'
-        }
-    })],
-    vite: {
-        plugins: [basicSsl()],
-        server: {
-            https: false
-        }
-    },
-    image: {
-        domains: ["a.storyblok.com"]
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      meta: 'storyblok/Meta',
+      image_text: 'components/modules/ImageText'
     }
+  })],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: false
+    }
+  },
+  image: {
+    domains: ["a.storyblok.com"]
+  },
+  output: "server",
+  adapter: vercel()
 });
